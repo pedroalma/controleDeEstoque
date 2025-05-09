d95c80f5873401c3971ff194e58eb2505f5fe299
pedroalma <pedroalma554@gmail.com>
2025-05-09 11:42:14 -0300

    novo2

--- script.js
+++ script.js
@@ -5,9 +5,37 @@ const preco = document.getElementById("preco");
 const quantidade = document.getElementById("quantidade");
 const imagem = document.getElementById("imagem");
 const produtoForm = document.getElementById("produto-form")
+const notificacao = document.getElementById("notificacao-conteudo");
 
 const categorias =[];
 const produtos =[];
+
+// escondendo a notificasao ate que a funcao seja chamada
+notificacao.style.display = 'none';
+
+function exibirNotificacao(mensagem,status){
+    
+    const messageEl = document.getElementById("notificacao-mensagem");
+
+    messageEl.textContent = mensagem;
+
+    if(status === 'sucesso'){
+        notificacao.style.backgroundColor = '#dbead5';
+        messageEl.style.color= '#103900';
+    } else if(status === 'alerta'){
+        notificacao.style.backgroundColor = '#ffffa0';
+        messageEl.style.color='#646600';
+    }else if(status == 'erro'){
+        notificacao.style.backgroundColor = '#fb6866';
+        messageEl.style.color='#470300'
+    }
+
+    notificacao.style.display = 'block';
+
+    setTimeout (() => {
+        notificacao.style.display = "none";
+    },3000);
+}
 
 produtoForm.addEventListener("submit", (event) => {
     //impedir de recarregar a pagina quando o evento de submit()  
@@ -25,7 +53,6 @@ produtoForm.addEventListener("submit", (event) => {
         camposPreenchidos = false;
     }else{
         document.getElementById("erro-categoria").style.display = "none";
-        camposPreenchidos = false;
     }
     if(preco.value == ""){
         document.getElementById("erro-preco").style.display = "block";
@@ -40,9 +67,6 @@ produtoForm.addEventListener("submit", (event) => {
         document.getElementById("erro-quantidade").style.display = "none";
     }
 
-    if(camposPreenchidos == false){
-        return
-    }
     const produtosInseridos = {
         nome: nome.value,
         categoria: categoria.value,
@@ -54,13 +78,14 @@ produtoForm.addEventListener("submit", (event) => {
     produtos.push(produtosInseridos);
     localStorage.setItem("nomeProduto",JSON.stringify(produtos));
 
-    // categorias.push(novaCategoria);
-    // produtos.push(novoProduto);
+    produtoForm.reset();
 
-    // localStorage.setItem("nomeProduto",nome.value);
-    // localStorage.setItem("nomeCategoria",categoria.value);
+    if(camposPreenchidos == false){
+        exibirNotificacao('Mensagem de alerta  erro ' , 'erro');
+       return
+   }else{
+       exibirNotificacao('Mensagem de alerta sucesso' , 'sucesso');
+       return
+   };
+   
 });
-
-// erro-categoria
-// erro-preco
-// erro-quantidade 
